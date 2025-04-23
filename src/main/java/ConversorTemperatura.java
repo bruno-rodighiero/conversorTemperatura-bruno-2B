d3da76ece6a344cd1ae9bbd308e011d58c02f1d6
Bruno Rodighiero <bruno.rodrighero@gmail.com>
2025-04-23 14:39:35 -0400

    Segundo Commit

--- src/main/java/ConversorTemperatura.java
+++ src/main/java/ConversorTemperatura.java
@@ -1,11 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-/**
- *
- * @author 1330626
- */
 public class ConversorTemperatura {
 
     public static void main(String[] args) {
